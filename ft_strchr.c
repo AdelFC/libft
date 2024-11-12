4871942fec03ee80d32a636481367da456fdd4c9
42 Adel FC <adel.fodilcherif@gmail.com>
2024-11-12 16:09:05 +0100

    MAJ + ADD ft_atoi

--- ft_strchr.c
+++ ft_strchr.c
@@ -6,8 +6,27 @@
 /*   By: afodil-c <afodil-c@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/12 11:29:43 by afodil-c          #+#    #+#             */
-/*   Updated: 2024/11/12 11:29:45 by afodil-c         ###   ########.fr       */
+/*   Updated: 2024/11/12 16:08:58 by afodil-c         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 char    *ft_strchr(const char *s, int c);
+
+char    *ft_strchr(const char *s, int c)
+{
+    
+}
+
+#include <stdio.h>
+#include <string.h>
+
+int	main(int argc, char **argv)
+{
+	if (argc == 2)
+	{
+		printf("The integer is %d. \n", atoi(argv[1]));
+        printf("With ft_atoi, integer is %d printable, function works. \n", ft_atoi(argv[1]));
+    }
+    else
+		return (0);
+}
