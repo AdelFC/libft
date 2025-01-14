e8d2f6e3c04cc711f7efe255be899e8980595f51
AdelFC <adel.fodil-cherif@gmail.com>
2025-01-14 19:29:41 +0100

    clean ft_memmove

--- ft_memmove.c
+++ ft_memmove.c
@@ -6,7 +6,7 @@
 /*   By: afodil-c <afodil-c@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/12 11:29:36 by afodil-c          #+#    #+#             */
-/*   Updated: 2024/12/02 10:54:16 by afodil-c         ###   ########.fr       */
+/*   Updated: 2025/01/14 19:24:26 by afodil-c         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,13 +16,11 @@ void	*ft_memmove(void *dst, const void *src, size_t len)
 {
 	char	*psrc;
 	char	*pdst;
-	size_t	i;
 
 	if (!dst && !src)
 		return (0);
 	psrc = (char *)src;
 	pdst = (char *)dst;
-	i = 0;
 	if (pdst > psrc)
 		while (len-- > 0)
 			pdst[len] = psrc[len];
